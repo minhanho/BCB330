b3795de1dc57d0d38c5c2b1c8ac9eb77e54fc0b6
Leon French <leonfrench@gmail.com>
2018-11-27 13:30:11 -0500

    A little bit of refactoring

--- imageManipulation.R
+++ imageManipulation.R
@@ -1,8 +1,13 @@
-install.packages("ImageMagick")
+#install.packages("magick")
+#install.packages("here")
 library(magick)
+library(here)
+
+dir.create(here("data/processed"))
 
 for (pageNum in 1:120){
-  cells <- image_read_pdf("/Users/minhanho/Documents/BCB330/Pictures/cell_images_all_heatmaporder_jun26_2014.pdf", pages=c(pageNum))
+  
+  cells <- image_read_pdf(here( "data/cell_images_all_heatmaporder_jun26_2014.pdf"), pages=c(pageNum))
   trimmed <- image_trim(cells)
   for(colNum in 1:5){
     dimCols <- paste("540x2750+", as.character(540*(colNum-1) + 12.5*(colNum-1)), sep="")
@@ -11,8 +16,11 @@ for (pageNum in 1:120){
       dimRows <- paste("540x540+0+", as.character(540*(rowNum-1) + 12.5*(rowNum-1)), sep="")
       cropRow <- image_crop(cropCol, dimRows)
       final <- image_crop(cropRow, "540x485+0+55")
-      cellPath <- paste("cell",as.character(colNum*rowNum*pageNum), ".pdf", sep="")
+      cellPath <- here("data", "processed", paste0("cell",as.character(colNum*rowNum*pageNum), ".pdf"))
       image_write(final, path= cellPath, format= "pdf")
+      if (!file.exists(cellPath)) {
+        stop("File not made")
+      }
     }
   }
 }
