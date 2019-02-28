5dd0a9cac9e18cec8568b795be0bec6f43b9c08c
Minh An Ho <minhan.ho@hotmail.com>
2019-02-28 01:53:56 -0500

    Processing csv

--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -4,8 +4,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
-tpot_data = pd.read_csv('/Users/minhanho/Documents/BCB330/TF_FeatureExtraction/features_with_level1class.csv',sep='COLUMN_SEPARATOR', converters={'cell_id': np.float64})
-features = tpot_data.drop('target', axis=1).values
+tpot_data = pd.read_csv('/Users/minhanho/Documents/BCB330/TF_FeatureExtraction/features_with_level1class.csv')
+first = tpot_data.drop('target', axis=1)
+second = first.drop('fullFilename', axis=1)
+features = second.drop('cell_id', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
