1c765c06d3660fc8d37819ddde5281610a09983f
Minh An Ho <minhan.ho@hotmail.com>
2019-03-12 01:50:31 -0400

    Unlabelled confusion matrix

--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('/Users/minhanho/Documents/BCB330/TF_FeatureExtraction/features_with_level1class.csv')
@@ -16,3 +18,6 @@ exported_pipeline = LogisticRegression(C=0.01, dual=False, penalty="l2")
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
+
+mat = confusion_matrix(testing_target, results)
+print(mat)
