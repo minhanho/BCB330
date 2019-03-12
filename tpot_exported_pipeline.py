12e632879c2f42aac39f797895832a28edba4b14
Minh An Ho <minhan.ho@hotmail.com>
2019-03-12 02:04:00 -0400

    Labelled and visualized confusion matrix - preliminary

--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+import pylab as plt
 
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -19,5 +20,20 @@ exported_pipeline = LogisticRegression(C=0.01, dual=False, penalty="l2")
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
 
-mat = confusion_matrix(testing_target, results)
+cell_types = []
+for i in tpot_data["target"]:
+    if i not in cell_types:
+        cell_types.append(i)
+
+mat = confusion_matrix(testing_target, results, cell_types)
 print(mat)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(mat)
+plt.title('Confusion matrix of the classifier')
+fig.colorbar(cax)
+ax.set_xticklabels([''] + cell_types)
+ax.set_yticklabels([''] + cell_types)
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.show()
